b2bcf7322dfb9279765bed5335c73c623c255c0e
jwhaosis <jwhaosis@gmail.com>
2018-04-23 23:24:16 -0400

    like and retweet

--- app.rb
+++ app.rb
@@ -12,25 +12,87 @@ require './models/tweettag'
 require './models/user'
 
 Dotenv.load
-uri = URI.parse(ENV["REDIS_URI"])
-$redis = Redis.new(:host => uri, :port => 10619, :password => ENV["REDIS_PASS"])
+$redis = Redis.new(:host => ENV["REDIS_URI"], :port => 10619, :password => ENV["REDIS_PASS"])
 
 get '/' do
-
+  "Welcome to the Scuteser database backend, why are you here?"
 end
 
-post '/async' do
-  sleep(10)
-end
-
-post '/' do
+post '/create/like' do
   request.body.rewind
-  data = request.body.read.to_s
-  # puts "-------------------#{request.media_type}---------------------------------"
-  # puts "-------------------#{request.env['rack.input'].read}---------------------"
-  # puts "--------------------#{data.class}------------------"
-  # puts "--------------------#{data}----------------------"
-  $redis.set("test", data)
+  model_params = JSON.parse request.body.read.to_s
+  tweet_id = model_params["tweet_id"]
+  tweet_info = eval $redis.get("tweet_info")
+  if tweet_info[tweet_id].nil?
+    tweet_info[tweet_id] = [1,0]
+  else
+    tweet_info[tweet_id][0] += 1
+  end
+  $redis.set("tweet_info", tweet_info)
+  redis_key = "cache_like"
+  model_array = $redis.get(redis_key)
+  if model_array.nil?
+    $redis.set(redis_key, [model_params])
+  else
+    $redis.set(redis_key, ((eval model_array).push model_params))
+  end
+end
+
+post '/delete/like' do
+  request.body.rewind
+  model_params = JSON.parse request.body.read.to_s
+  tweet_id = model_params["tweet_id"]
+  tweet_info = eval $redis.get("tweet_info")
+  if tweet_info[tweet_id].nil?
+    tweet_info[tweet_id] = [0,0]
+  else
+    tweet_info[tweet_id][0] -= 1
+  end
+  $redis.set("tweet_info", tweet_info)
+  redis_key = "cache_like"
+  model_array = $redis.get(redis_key)
+  if !model_array.nil?
+    $redis.set(redis_key, ((eval model_array).delete model_params))
+  end
+end
+
+
+post '/create/retweet' do
+  request.body.rewind
+  model_params = JSON.parse request.body.read.to_s
+  tweet_id = model_params["retweet_id"]
+  tweet_info = eval $redis.get("tweet_info")
+  if tweet_info[tweet_id].nil?
+    tweet_info[tweet_id] = [0,1]
+  else
+    tweet_info[tweet_id][1] += 1
+  end
+  $redis.set("tweet_info", tweet_info)
+  redis_key = "cache_retweet"
+  model_array = $redis.get(redis_key)
+  if model_array.nil?
+    $redis.set(redis_key, [model_params])
+  else
+    $redis.set(redis_key, ((eval model_array).push model_params))
+  end
+end
+
+post '/delete/retweet' do
+  request.body.rewind
+  model_params = JSON.parse request.body.read.to_s
+  tweet_id = model_params["retweet_id"]
+  tweet_info = eval $redis.get("tweet_info")
+  if tweet_info[tweet_id].nil?
+    tweet_info[tweet_id] = [0,0]
+  else
+    tweet_info[tweet_id][1] -= 1
+  end
+  $redis.set("tweet_info", tweet_info)
+  redis_key = "cache_retweet"
+  model_array = $redis.get(redis_key)
+  if !model_array.nil?
+    $redis.set(redis_key, ((eval model_array).delete model_params))
+  end
 end
 
 post '/create/:model' do
