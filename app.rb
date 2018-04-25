33b25dc684ce8a7c0ff9aedc6d684a2c28bd8d79
jwhaosis <jwhaosis@gmail.com>
2018-04-25 17:09:16 -0400

    tweet post changes

--- app.rb
+++ app.rb
@@ -31,13 +31,14 @@ get '/create/tweet/:id' do
   id = $redis.get("tweet_inc")
   $redis.incr "tweet_inc"
   new_tweet = Tweet.create(id: id, user_id: params[:id], tweet: body, created_at: Time.now)
+  new_tweet = (JSON.parse new_tweet.to_json)["name"] = User.where(id: id).first.name
   key = "user#{id}_tweets"
   user_tweets = $redis.get(key)
   if user_tweets.nil?
     user_tweets = Tweet.joins(:user).where(user_id: id).select("tweets.*, users.name").order(:created_at).last(50).reverse.to_json
   else
     user_tweets = JSON.parse user_tweets
-    user_tweets.pop
+    user_tweets.pop if(user_tweets.size > 49)
     user_tweets.unshift new_tweet
   end
   $redis.set(key, user_tweets.to_json)
---
+++ db/schema.rb
@@ -0,0 +1,58 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_02_20_200832) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "followers", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "followed_by_id"
+  end
+
+  create_table "hashtags", force: :cascade do |t|
+    t.string "hashtag"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.index ["tweet_id"], name: "index_likes_on_tweet_id"
+  end
+
+  create_table "mentions", force: :cascade do |t|
+    t.integer "tweet_id"
+    t.integer "mentioned_user_id"
+  end
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "tweet"
+    t.datetime "created_at"
+    t.integer "user_id"
+    t.integer "retweet_id"
+    t.index ["retweet_id"], name: "index_tweets_on_retweet_id"
+  end
+
+  create_table "tweettags", force: :cascade do |t|
+    t.integer "hashtag_id"
+    t.integer "tweet_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.string "remember_digest"
+  end
+
+end
