e56f7720b6d9c33aec1effef8f1f37fcc305ce02
jwhaosis <jwhaosis@gmail.com>
2018-04-25 16:52:02 -0400

    user change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,4 @@
 class User < ActiveRecord::Base
-
-  attr_accessor :remember_token
-  before_save { email.downcase! }
-  validates :name, presence: true, length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 },
-            format: { with: VALID_EMAIL_REGEX },
-            uniqueness: { case_sensitive: false }
-  has_secure_password validations: false
   has_many :tweets
   has_many :following_someone, class_name: 'Follower', foreign_key: 'followed_by_id'
   has_many :followed_by_someone, class_name: 'Follower', foreign_key: 'user_id'
